22646db3e6b51935d9c32adb8b9a3057d74dfd56
pavanatest <112800029+pavanatest@users.noreply.github.com>
2022-09-11 11:25:06 +0530

    Add files via upload

---
+++ DuplicateLead.java
@@ -0,0 +1,91 @@
+package week2.day1;
+
+//port org.apache.commons.math3.distribution.GammaDistribution;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class DuplicateLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+				WebDriverManager.chromedriver().setup();
+				String driverPath = System.getProperty("webdriver.chrome.driver");
+				System.out.println(driverPath);
+				
+				ChromeDriver driver = new ChromeDriver();
+				
+				driver.get("http://leaftaps.com/opentaps/control/main");
+				driver.manage().window().maximize();
+				
+				driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+						
+				
+				 
+				driver.findElement(By.id("password")).sendKeys("crmsfa");
+				
+				driver.findElement(By.className("decorativeSubmit")).click();
+				
+				driver.findElement(By.linkText("CRM/SFA")).click();
+				driver.findElement(By.linkText("Leads")).click();
+				driver.findElement(By.linkText("Create Lead")).click();
+				driver.findElement(By.id("createLeadForm_companyName")).sendKeys("testLeaf");
+				driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Pavana");
+				driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+				driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Pavana");
+				driver.findElement(By.name("departmentName")).sendKeys("Software testing");
+				driver.findElement(By.name("description")).sendKeys("Testing");
+				driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("papu@gmail.com");
+				WebElement selector=driver.findElement(By.name("generalStateProvinceGeoId"));
+				Select g=new Select(selector);
+				g.selectByVisibleText("New York");
+				driver.findElement(By.name("submitButton")).click();
+				String title = driver.getTitle();
+				System.out.println(title);
+				
+				if(title.equals("View Lead | opentaps CRM"))
+					System.out.println("title is matching");
+				else
+					System.out.println("title is not matching");
+				
+				driver.findElement(By.linkText("Duplicate Lead")).click();
+				
+				
+				
+				WebElement name1=driver.findElement(By.id("createLeadForm_companyName"));
+				name1.clear();
+				name1.sendKeys("TCS");
+				
+				WebElement name2=driver.findElement(By.id("createLeadForm_firstName"));
+				name2.clear();
+				name2.sendKeys("Papu");
+				
+				driver.findElement(By.className("smallSubmit")).click();
+				
+			String dupTitle = driver.getTitle();
+				System.out.println(dupTitle);
+			
+			if(dupTitle.equals("View Lead |opentaps CRM"))
+				System.out.println("title is matching");
+			else
+				System.out.println("title is not matching");
+						
+				
+				
+				
+				
+
+			}
+
+		}
+
+		
+
+	
+
+
